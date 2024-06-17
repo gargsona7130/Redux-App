97ed4e4382c24943afe00674d10c38c7e64ab5e1
gargsona7130 <gargsonal7130@gmail.com>
2024-06-17 15:22:35 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import "./App.css"
 const App=()=>{
   return(
     <>
-    <div class="container">
+    <div className="container">
 
     <h1> INCREMENT/DECREMENT COUNTER </h1>
     <h4> Using React and Redux </h4>
 
-<div class="quantity">
-  <a class="quantity_minus" title="Decrement"><span> - </span></a>
-  <input name="quantity" type="text" class="quantity__input" value="0"/>
-  <a class="quantity_plus" title="Increment"><span> + </span></a>
+<div className="quantity">
+  <a className="quantity_minus" title="Decrement"><span> - </span></a>
+  <input name="quantity" type="text" className="quantity__input"/>
+  <a className="quantity_plus" title="Increment"><span> + </span></a>
 </div>
     </div>
     
